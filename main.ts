eec6fc6775120a34b3f2d35a2f416826d535a82f
MalcolmPot <64169782+MalcolmPot@users.noreply.github.com>
2020-09-10 16:38:28 +0100

    Update pxt.json, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     basic.showLeds(`
         . # . # .
         . # . # .
@@ -7,9 +7,9 @@ input.onButtonPressed(Button.A, function () {
         . # # # .
         `)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     basic.showString("Hello!")
 })
-basic.forever(function () {
-	
+basic.forever(function on_forever() {
+    
 })
